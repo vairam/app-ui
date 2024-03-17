5fe7a98ac537755da3b02616e5579d85cc1250c0
Vairamuthu Sethu <vsethu@accurate.com>
2024-03-17 10:50:44 +0530

    Initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,11 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Button } from "app-lib";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
@@ -18,6 +17,7 @@ function App() {
         >
           Learn React
         </a>
+        <Button>I am a button</Button>
       </header>
     </div>
   );
